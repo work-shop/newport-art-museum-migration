2258e2aabae74c25c5280cf1d77f1d24b1856fd6
Nic Schumann <nicolas_schumann@brown.edu>
2018-07-05 14:02:25 -0400

    [Fields] Renamed Fields to exact NPSP mapping terms.

--- fields.js
+++ fields.js
@@ -78,101 +78,104 @@ fields.NPSPImport = {
     headers: [
 
         // A Household will be created for Contact1
-        'Contact1 Salutation __c',
-        'Contact1 First Name __c',
-        'Contact1 Last Name __c', // Required, if any Contact1 info present
-        'Contact1 Birthdate __c',
-        'Contact1 Title __c',
-        'Contact1 Personal Email __c',
-        'Contact1 Work Email __c',
-        'Contact1 Alternate Email __c',
-        'Contact1 Preferred Email __c', // One of Personal, Work, or Alternate, if specified
-        'Contact1 Home Phone __c',
-        'Contact1 Work Phone __c',
-        'Contact1 Mobile Phone __c',
-        'Contact1 Other Phone __c',
-        'Contact1 Preferred Phone __c', // One of Home, Work, Mobile, or Other, if specified
-        'Contact1 RE ID __c',
-        'Contact1 Solicit Codes __c', // Solicit Codes for Contact1
-        'Contact1 Constituent Codes __c',
-        'Contact1 Gender __c',
+        'Contact1 Salutation',
+        'Contact1 First Name',
+        'Contact1 Last Name', // Required, if any Contact1 info present
+        'Contact1 Birthdate',
+        'Contact1 Title',
+        'Contact1 Personal Email',
+        'Contact1 Work Email',
+        'Contact1 Alternate Email',
+        'Contact1 Preferred Email', // One of Personal, Work, or Alternate, if specified
+        'Contact1 Home Phone',
+        'Contact1 Work Phone',
+        'Contact1 Mobile Phone',
+        'Contact1 Other Phone',
+        'Contact1 Preferred Phone', // One of Home, Work, Mobile, or Other, if specified
+        'Contact1 RE ID',
+        'Contact1 Solicit Codes', // Solicit Codes for Contact1
+        'Contact1 Constituent Codes',
+        'Contact1 Gender',
 
         // These will be the address data associated with the Household created for Contact1
-        'Home Street __c',
-        'Home City __c',
-        'Home State/Province __c',
-        'Home Zip/Postal Code __c',
+        'Home Street',
+        'Home City',
+        'Home State/Province',
+        'Home Zip/Postal Code',
         'Home Country',
 
         // Contact2 will be connected to Contact1's Household
-        'Contact2 Salutation __c',
-        'Contact2 First Name __c',
-        'Contact2 Last Name __c', // Required, if any Contact2 info present
-        'Contact2 Birthdate __c',
-        'Contact2 Title __c',
-        'Contact2 Personal Email __c',
-        'Contact2 Work Email __c',
-        'Contact2 Alternate Email __c',
-        'Contact2 Preferred Email __c', // One of Personal, Work, or Alternate, if specified
-        'Contact2 Home Phone __c',
-        'Contact2 Work Phone __c',
-        'Contact2 Mobile Phone __c',
-        'Contact2 Other Phone __c',
-        'Contact2 Preferred Phone __c', // One of Home, Work, Mobile, or Other, if specified
-        'Contact2 RE ID __c',
-        'Contact2 Solicit Codes __c',
-        'Contact2 Constituent Codes __c',
-        'Contact2 Gender __c',
+        'Contact2 Salutation',
+        'Contact2 First Name',
+        'Contact2 Last Name', // Required, if any Contact2 info present
+        'Contact2 Birthdate',
+        'Contact2 Title',
+        'Contact2 Personal Email',
+        'Contact2 Work Email',
+        'Contact2 Alternate Email',
+        'Contact2 Preferred Email', // One of Personal, Work, or Alternate, if specified
+        'Contact2 Home Phone',
+        'Contact2 Work Phone',
+        'Contact2 Mobile Phone',
+        'Contact2 Other Phone',
+        'Contact2 Preferred Phone', // One of Home, Work, Mobile, or Other, if specified
+        'Contact2 RE ID',
+        'Contact2 Solicit Codes',
+        'Contact2 Constituent Codes',
+        'Contact2 Gender',
 
         // Account1 will be related to Contact1
-        'Account1 Name __c', // Required, if any other fields from Account1 are present
-        'Account1 Street __c',
-        'Account1 City __c',
-        'Account1 State/Province __c',
-        'Account1 Zip/Postal Code __c',
-        'Account1 Country __c',
-        'Account1 Phone __c',
-        'Account1 Website __c', // Skip
-        'Account1 RE ID __c',
-        'Account1 Solicit Codes __c',
-        'Account1 Constituent Codes __c',
+        'Account1 Name', // Required, if any other fields from Account1 are present
+        'Account1 Street',
+        'Account1 City',
+        'Account1 State/Province',
+        'Account1 Zip/Postal Code',
+        'Account1 Country',
+        'Account1 Phone',
+        'Account1 Website', // Skip
+        'Account1 RE ID',
+        'Account1 Solicit Codes',
+        'Account1 Constituent Codes',
 
         // Account2 will be related to Contact2
-        'Account2 Name __c', // Required, if any other fields from Account1 are present
-        'Account2 Street __c',
-        'Account2 City __c',
-        'Account2 State/Province __c',
-        'Account2 Zip/Postal Code __c',
-        'Account2 Country __c',
-        'Account2 Phone __c',
-        'Account2 Website __c', // Skip
-        'Account2 RE ID __c',
-        'Account2 Solicit Codes __c',
-        'Account2 Constituent Codes __c',
+        'Account2 Name', // Required, if any other fields from Account1 are present
+        'Account2 Street',
+        'Account2 City',
+        'Account2 State/Province',
+        'Account2 Zip/Postal Code',
+        'Account2 Country',
+        'Account2 Phone',
+        'Account2 Website', // Skip
+        'Account2 RE ID',
+        'Account2 Solicit Codes',
+        'Account2 Constituent Codes',
 
         // Donation Information
-        'Donation Donor __c', // either Contact1 to associate the donation with Contact1 or 'Account1' to associate with the org account. Defaults to 'Contact1'
-        'Donation Amount __c', // Required
-        'Donation Date __c', // The date you received the donation. In RE, 'Gift Date', not 'Post Date'
-        'Donation Name __c', // a unique name for the donation. Leave empty
-        'Donation Record Type Name __c', // The Opportunity Record Type to use –
-        'Donation Type __c', // Donation Type, as per RE Gift Type: // Cash, Pledge, Stock, Gift In Kind, Other, Recurring Gift, Art, Bequest
-        'Donation Stage __c', // The Stage of the donation – Defaults to Closed/Won Stage
-        'Donation Description __c', // Optional Description of the Donation. Leave Blank
-        'Donation Member Level __c', // The Member Level (accodring to RE) corresponding to a particular donation
-        'Donation Membership Origin __c', // Not sure what this is for. Skip for now.
-        'Donation Membership Start Date __c', // The start date of the membership if the donation is for a membership.
-        'Donation Membership End Date __c', // The end date of the membership if the donation is for a membership.
-        'Donation Campaign Name __c',
-        'Donation RE Campaign __c', // Tries to match an existing campaign by name. If no match is found, will create a new campaign.
-        'Donation RE Appeal __c',
-        'Donation RE Fund __c',
-        'Donation RE ID __c',
-        'Donation RE Membership ID __c',
+        'Donation Donor', // either Contact1 to associate the donation with Contact1 or 'Account1' to associate with the org account. Defaults to 'Contact1'
+        'Donation Amount', // Required
+        'Donation Date', // The date you received the donation. In RE, 'Gift Date', not 'Post Date'
+        'Donation Name', // a unique name for the donation. Leave empty
+        'Donation Record Type Name', // The Opportunity Record Type to use –
+        'Donation Type', // Donation Type, as per RE Gift Type: // Cash, Pledge, Stock, Gift In Kind, Other, Recurring Gift, Art, Bequest
+        'Donation Stage', // The Stage of the donation – Defaults to Closed/Won Stage
+        'Donation Description', // Optional Description of the Donation. Leave Blank
+        'Donation Member Level', // The Member Level (accodring to RE) corresponding to a particular donation
+        'Donation Membership Origin', // Not sure what this is for. Skip for now.
+        'Donation Membership Start Date', // The start date of the membership if the donation is for a membership.
+        'Donation Membership End Date', // The end date of the membership if the donation is for a membership.
+        'Donation Campaign Name',
+        'Donation RE Campaign', // Tries to match an existing campaign by name. If no match is found, will create a new campaign.
+        'Donation RE Appeal',
+        'Donation RE Fund',
+        'Donation RE ID',
+        'Donation RE Membership ID',
         'Donation Acknowledgement Status',
-        'Payment Method __c', // The pay-method for this donation
-        'Payment Date __c',
-        'Payment Check/Reference Number __c' // The check or ref number, if there is one.
+        'Payment Method', // The pay-method for this donation
+        'Payment Date',
+        'Payment Check/Reference Number', // The check or ref number, if there is one.
+
+        'FLAG: Bad Address',
+        'FLAG: Duplicate Record'
     ]
 };
 
